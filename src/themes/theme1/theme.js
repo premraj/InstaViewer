14258aa858e575f905e0d6c091c8342c674a3e8b
raj.prem <raj.prem@Raj-MacBook-Pro.local>
2020-07-17 02:50:20 +0800

    Move colors out

--- src/themes/theme1/theme.js
+++ src/themes/theme1/theme.js
@@ -1,15 +1,17 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
+const colors = {
+  c01: '#0C1118',
+  c02: '#1A2639',
+  c03: '#FFFFFF',
+  c04: '#C24D2C',
+  c05: '#D9DAD7',
+  c06: '#1A2639',
+  c07: '#3E4A61',
+};
+
 const theme = createMuiTheme({
-  colors: {
-    c01: '#0C1118',
-    c02: '#1A2639',
-    c03: '#FFFFFF',
-    c04: '#C24D2C',
-    c05: '#D9DAD7',
-    c06: '#1A2639',
-    c07: '#3E4A61',
-  },
+  colors,
   fontFamily: 'Lato',
   mq: {
     Up800: '(min-width:800px)',
